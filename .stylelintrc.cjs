1becb8b499197b938be5bd0e4fc77654eb7ab556
RenaudBresson <120473987+Renaud-CCI@users.noreply.github.com>
2025-10-19 16:41:01 +0200

    install tailwind

---
+++ .stylelintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    // Allow Tailwind at-rules
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen', 'layer'],
+      },
+    ],
+  },
+}
